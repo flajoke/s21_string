9a6746a3dc75c3f318716d1709ad1c19bb05fcb1
Perros Deon <perrosde@pr-b8.nsk.21-school.ru>
2023-01-16 17:57:43 +0700

    ADD workspace

---
+++ src/memory/main.c
@@ -0,0 +1,18 @@
+#include "memory.h"
+
+int main(){
+
+    return 0;
+}
+
+int main () {
+   const char str[] = "http://www.tutorialspoint.com";
+   const char ch = '.';
+   char *ret;
+
+   ret = memchr(str, ch, strlen(str));
+
+   printf("String after |%c| is - |%s|\n", ch, ret);
+
+   return(0);
+}
---
+++ src/memory/memory.h
@@ -0,0 +1,10 @@
+#ifndef S21_MEMORY
+#define S21_MEMORY
+
+#include <stdio.h>  
+
+void foo();
+
+#include <string.h>
+
+#endif //S21_MEMORY
