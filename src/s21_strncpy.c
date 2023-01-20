97b2c758961a962c10bb768e47389ca740f67873
flajoke <flajoke@flajoke-vm.localdomain>
2023-01-20 22:18:18 +0700

    by_schrader

---
+++ src/s21_strncpy.c
@@ -0,0 +1,13 @@
+#include "s21_string.h"
+
+char *s21_strncpy(char *dest, const char *src, size_t n) {
+    s21_size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    while (i < n) {
+        dest[i] = '\0';
+        i++;
+    }
+    return dest;
+}
