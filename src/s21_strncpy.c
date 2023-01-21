d5e73104fb6fc8dfe16b55fd03b5915f0f21e7b5
flajoke <flajoke@flajoke-vm.localdomain>
2023-01-21 23:01:17 +0700

    by schrader

--- src/s21_strncpy.c
+++ src/s21_strncpy.c
@@ -1,7 +1,9 @@
+// Копирует до n символов из строки, на которую указывает src, в dest.
+
 #include "s21_string.h"
 
 char *s21_strncpy(char *dest, const char *src, size_t n) {
-    s21_size_t i;
+    size_t i;
     for (i = 0; i < n && src[i] != '\0'; i++) {
         dest[i] = src[i];
     }
