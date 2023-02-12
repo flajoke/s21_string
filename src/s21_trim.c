985c31542866627abd30f774c17bb6a3c47ccf64
flajoke <110658464+flajoke@users.noreply.github.com>
2023-02-13 05:53:20 +0700

    denie me too

--- src/s21_trim.c
+++ src/s21_trim.c
@@ -7,51 +7,6 @@
 // из данной строки (src). В случае какой-либо 
 // ошибки следует вернуть значение NULL
 
-// void* s21_trim(const char* src, const char* trim_chars) {
-//     char* res = NULL;
-//     if (src && trim_chars) {
-//         char* ptr_str = (char*)src;
-//         char* end_str = (char*)src + s21_strlen(src);
-//         for (; *ptr_str && s21_strchr(trim_chars, *ptr_str); ptr_str++) ;
-//         for (; end_str != ptr_str && s21_strchr(trim_chars, *(end_str - 1)); end_str--) ;
-//         res = (char*)malloc((end_str - ptr_str + 1) * sizeof(char));
-//         if (res) {
-//             s21_strncpy(res, ptr_str, end_str - ptr_str);
-//             *(res + (end_str - ptr_str)) = '\0';
-//         }
-//     }
-//     return res;
-// }
-
-// void *s21_trim(const char *src, const char *trim_chars) {
-//   if (!src || !trim_chars) {
-//     return NULL;
-//   }
-
-//   int start = 0, end = strlen(src) - 1;
-//   while (start <= end && strchr(trim_chars, src[start])) {
-//     start++;
-//   }
-//   while (end >= start && strchr(trim_chars, src[end])) {
-//     end--;
-//   }
-
-//   int length = end - start + 1;
-//   if (length <= 0) {
-//     length = 1;
-//   }
-
-//   char *trimmed = malloc(length + 1);
-//   if (!trimmed) {
-//     return NULL;
-//   }
-
-//   memcpy(trimmed, src + start, length);
-//   trimmed[length] = '\0';
-
-//   return trimmed;
-// }
-
 void *s21_trim(const char *src, const char *trim_chars) {
     char *res = NULL;
 
