816c83bf57a924ac58190a632419586e7c01de6e
flajoke <110658464+flajoke@users.noreply.github.com>
2023-02-13 05:50:59 +0700

    denie me too

--- src/s21_strpbrk.c
+++ src/s21_strpbrk.c
@@ -1,22 +1,5 @@
 #include "s21_string.h"
 
-// char *s21_strpbrk(const char *s, const char *charset) {
-//   size_t i;
-//   char match = 0;
-//   for (i = 0; s[i] != '\0'; i++) {
-//     for (size_t j = 0; charset[j] != '\0'; j++) {
-//       if (s[i] == charset[j]) {
-//         match = 1;
-//         break;
-//       }
-//     }
-//     if (match) {
-//       break;
-//     }
-//   }
-//   return (char *)(s + i);
-// }
-
 char *s21_strpbrk(const char *str1, const char *str2) {
     const char *s1, *s2;
     for (s1 = str1; *s1 != '\0'; s1++) {
